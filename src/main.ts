c131a71eada2588d6b113db43e09a8771fa9b1f6
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2025-05-10 12:37:10 +0545

    feat: add express as a dependency and set up a basic server
    - Added express to package.json dependencies.
    - Created a simple Express server in main.ts that responds with "Hello World!" on the root route.
    - Configured the server to listen on a specified port.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-import {mp4UrlToWav} from './utils/convertMp4ToWav.js';
-(async () => {
-  const audioPath = await mp4UrlToWav('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4');
-  console.log('WAV audio saved at:', audioPath);
-})();
+
+import express from 'express';
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.listen(PORT,()=>{
+    console.log(
+        "App is running on port",
+        PORT,
+    )
+})
